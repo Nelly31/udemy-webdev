4f92cb516bd41a99917e77487821adf7a4a2906d
Nelly Main <45417867+Nelly31@users.noreply.github.com>
2019-10-14 12:29:39 -0400

    Add files via upload

---
+++ jquery-methods.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  //text
+  $('h1').text('jQuery Methods')
+  //html
+  $('ul').html('<li>Mars Bars</li><li>M&M</li>')
+  //attributes
+  $('img').css('width: 50px')
+  $('img').attr('src', 'https://cdn.akc.org/Marketplace/Breeds/Saint_Bernard_SERP.jpg')
+})
+
